289775c61803ca3285f9fb97f50af4432ba89ee6
Kenneth <kenneth@knights.ucf.edu>
2020-06-04 01:49:07 -0400

    Implemented initial test framework

---
+++ define.hpp
@@ -0,0 +1,7 @@
+#ifndef DEFINE_HPP
+#define DEFINE_HPP
+
+#define NUM_OPS 10000
+#define THREAD_COUNT 1
+
+#endif
---
+++ main.hpp
@@ -0,0 +1,26 @@
+#ifndef __MAIN_HEADER__
+#define __MAIN_HEADER__
+
+#define TIME_UNIT nanoseconds
+
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+
+// Used to set process priority in Linux.
+#include <sys/resource.h>
+#include <unistd.h>
+
+#include "define.hpp"
+#include "hashMap.hpp"
+
+ConcurrentHashMap<void *, void *> hashMap;
+
+void threadRunner(std::thread *threads, void function(int threadNum));
+
+void preinsert(int threadNum);
+
+int setMaxPriority();
+
+#endif
