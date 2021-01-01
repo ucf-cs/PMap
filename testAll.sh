8103445ac0bc73a6f0f4364dce7c87f1ecdca8fd
Kenneth Lamar <kenneth@knights.ucf.edu>
2021-01-01 04:45:51 -0500

    Level hashing and PMap hashing now use the same initial capacity

--- testAll.sh
+++ testAll.sh
@@ -23,7 +23,8 @@ do
             if [[ $ds = "clevelDef" ]]; then
                 pmempool create obj --layout="clevel_hash" --size 1G /mnt/pmem/pm1/persistFile.bin
             fi
-            ./bin/test.out -t $i -n 400000 -p 1 -c 22 -f /mnt/pmem/pm1/persist.bin -r 0 -w 0
+            # 2^14 is the initial capacity of level hashing. Match it for other structures here.
+            ./bin/test.out -t $i -n 400000 -p 1 -c 14 -f /mnt/pmem/pm1/persist.bin -r 0 -w 0
         done
     done
 done
