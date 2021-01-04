751f9546c50780afad086c60610c8c14be024d9c
Kenneth Lamar <kenneth@knights.ucf.edu>
2021-01-03 21:33:09 -0500

    Fixed test thread counts Reduced test size to 50000 operations Added a check that the mapped file is actually in PMEM

--- testAll.sh
+++ testAll.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-DATA_STRUCTURES=(ucfDef stlDef pmDef onefileDef clevelDef)
-TESTS=(alternatingTestDef degreeTestDef randomTestDef)
+DATA_STRUCTURES=(ucfDef clevelDef stlDef pmDef onefileDef)
+TESTS=(redditTestDef)
 
 for t in "${TESTS[@]}";
 do
     for ds in "${DATA_STRUCTURES[@]}";
     do
-        if [[ $ds = "degreeTestDef" ]]; then
+        if [[ $t = "degreeTestDef" ]]; then
             MAX=4
-        elif [[ $ds = "redditTestDef" ]]; then
+        elif [[ $t = "redditTestDef" ]]; then
             MAX=1
         else
             MAX=80
         fi
+
         for ((i=1;i<=MAX;i++));
         do
             make clean && make DEFINES="-D$ds -D$t"
@@ -24,7 +25,7 @@ do
                 pmempool create obj --layout="clevel_hash" --size 1G /mnt/pmem/pm1/persistFile.bin
             fi
             # 2^14 is the initial capacity of level hashing. Match it for other structures here.
-            ./bin/test.out -t $i -n 400000 -p 1 -c 14 -f /mnt/pmem/pm1/persist.bin -r 0 -w 0
+            ./bin/test.out -t $i -n 50000 -p 1 -c 14 -f /mnt/pmem/pm1/persist.bin -r 0 -w 0
         done
     done
 done
